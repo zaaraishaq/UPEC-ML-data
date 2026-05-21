dc4cc627602104edbd33dec613e9d5139f846207
zaaraishaq <129344076+zaaraishaq@users.noreply.github.com>
2026-05-21 18:12:05 +0500

    Add files via upload

---
+++ Supplementary Table 1.js
@@ -0,0 +1,207 @@
+// Auto-generated from Supplementary Table 1.csv
+window.Supplementary_Table_1 = [
+  {
+    "Isolate ID": "A1.S1.126",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "A1.S1.18 & 42GP",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "BWH_139_E_col_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_883_E_col_1",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "BWH_205_E_col_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "CMZE095 & CMZE104",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "BWH_218_E_col_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_216_E_col_1",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "CMZE071",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "MRSN488642",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "CMZE093",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "CMZE108",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "CMZE119",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "MRSN387404",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "PNUECU56",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "PNUECU61 & BWH_365_E_col_1",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "PNUECU61",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "PNUECU56 & BWH_365_E_col_1",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "PNUECU70",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_365_E_col_1",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "RECU206",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "5EC",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "UPEC_ECZMYU21",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "EC1O25bH4ST131",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "VUTI202",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "VUTI325",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "VUTI202_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "CMZE098",
+    "Inferred Clade / Lineage Association": "ST131",
+    "Interpretation": "ST131 Variant"
+  },
+  {
+    "Isolate ID": "BWH_998_E_col_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "28Eco12",
+    "Inferred Clade / Lineage Association": "ST38",
+    "Interpretation": "ST38 Variant"
+  },
+  {
+    "Isolate ID": "CMZE043",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "GU27",
+    "Inferred Clade / Lineage Association": "ST38",
+    "Interpretation": "ST38 Variant"
+  },
+  {
+    "Isolate ID": "VUTI134",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "VUTI371",
+    "Inferred Clade / Lineage Association": "ST38",
+    "Interpretation": "ST38 Variant"
+  },
+  {
+    "Isolate ID": 12693,
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_1299_E_col_1",
+    "Inferred Clade / Lineage Association": "ST12",
+    "Interpretation": "ST12 Variant"
+  },
+  {
+    "Isolate ID": "VUTI180",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "12693 (Clustered with ST12)",
+    "Inferred Clade / Lineage Association": "ST12",
+    "Interpretation": "ST12 Variant"
+  },
+  {
+    "Isolate ID": "PAT23-2_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "PAT23-3_1",
+    "Inferred Clade / Lineage Association": "ST95",
+    "Interpretation": "ST95 Variant"
+  },
+  {
+    "Isolate ID": "26EC",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "13KWH46",
+    "Inferred Clade / Lineage Association": "ST602",
+    "Interpretation": "ST602 Variant"
+  },
+  {
+    "Isolate ID": "D1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "E_coli_N15-72",
+    "Inferred Clade / Lineage Association": "ST602",
+    "Interpretation": "ST602 Variant"
+  },
+  {
+    "Isolate ID": "BWH_1858_E_col",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "AR_Bank_0349",
+    "Inferred Clade / Lineage Association": "ST457",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "BWH_1889_E_col",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "MRSN832660",
+    "Inferred Clade / Lineage Association": "ST62",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "BWH_1903_E_col",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "PNUECU28 (Near PAT3-3)",
+    "Inferred Clade / Lineage Association": "ST744",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "PNUECU28",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_1903_E_col (Near PAT3-3)",
+    "Inferred Clade / Lineage Association": "ST744",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "MSHS_133",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "PNUECU16",
+    "Inferred Clade / Lineage Association": "ST1163",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "VUTI404",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_1300_E_col_1",
+    "Inferred Clade / Lineage Association": "ST963",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "GHA_BE_NMIMRSEQ_2021_1",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "BWH_351 / BWH_761",
+    "Inferred Clade / Lineage Association": "ST5614 / ST443",
+    "Interpretation": "Minor ST Variant"
+  },
+  {
+    "Isolate ID": "ATCC_700415",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "Deep branching outgroup",
+    "Inferred Clade / Lineage Association": "Divergent",
+    "Interpretation": "Rapidly Evolving / Hybrid"
+  },
+  {
+    "Isolate ID": "B51",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "H38 (Deep outgroup branch)",
+    "Inferred Clade / Lineage Association": "Divergent",
+    "Interpretation": "Rapidly Evolving / Hybrid"
+  },
+  {
+    "Isolate ID": "H38",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "B51 (Deep outgroup branch)",
+    "Inferred Clade / Lineage Association": "Divergent",
+    "Interpretation": "Rapidly Evolving / Hybrid"
+  },
+  {
+    "Isolate ID": "EC1390",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "Deep branching outgroup",
+    "Inferred Clade / Lineage Association": "Divergent",
+    "Interpretation": "Rapidly Evolving / Hybrid"
+  },
+  {
+    "Isolate ID": "VUTI215",
+    "Closest Phylogenetic Sister Taxa (from Tree)": "Deep branching outgroup",
+    "Inferred Clade / Lineage Association": "Divergent",
+    "Interpretation": "Rapidly Evolving / Hybrid"
+  }
+];
